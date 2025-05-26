1a1eb03783d2fd60221a77b1e5e840e85a2d3bc7
Tiago <tiago.m.gameiro@gmail.com>
2025-05-26 14:37:23 +0200

    pagination

--- routes/offers.js
+++ routes/offers.js
@@ -1,17 +1,82 @@
 var express = require("express");
 var router = express.Router();
-
 require("../models/connection");
 const Offer = require("../models/offers.js");
+const { checkBody } = require("../modules/checkBody");
 
+router.get("/", (req, res) => {
+  const { offset, limit } = req.query;
+  console.log(req.query);
 
+//   Offer.countDocuments();
 
-
-router.get('/', (req, res) => {
-	Offer.find().then(data => {
-		res.json({ offers: data });
-	});
+  Offer.find()
+    .skip(offset)
+    .limit(limit)
+    .then((data) => {
+      res.json({ offers: data });
+    });
 });
 
+router.post("/add", (req, res) => {
+  // Check if the required fields are present
+  if (
+    !checkBody(req.body, [
+      "title",
+      "compagny",
+      "grade",
+      "contractType",
+      "publicationDate",
+      "streetNumber",
+      "streetName",
+      "city",
+      "zipCode",
+      "source",
+      "offerLink",
+      "description",
+    ])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  // Create a new offer
+
+  Offer.findOne({ offerLink: req.body.offerLink })
+    .then((data) => {
+      if (data) {
+        // If the offer already exists, return an error
+        res.json({ result: false, error: "Offer already exists" });
+        return;
+      }
+    })
+    .catch((error) => {
+      res.json({ result: false, error: error.message });
+      return;
+    });
+  const newOffer = new Offer({
+    title: req.body.title,
+    compagny: req.body.compagny,
+    logoLink: req.body.logoLink,
+    grade: req.body.grade,
+    contractType: req.body.contractType,
+    publicationDate: req.body.publicationDate,
+    streetNumber: req.body.streetNumber,
+    streetName: req.body.streetName,
+    city: req.body.city,
+    zipCode: req.body.zipCode,
+    source: req.body.source,
+    offerLink: req.body.offerLink,
+    description: req.body.description,
+  });
+  // Save the offer to the database
+  newOffer
+    .save()
+    .then(() => {
+      res.json({ result: true });
+    })
+    .catch((error) => {
+      res.json({ result: false, error: error.message });
+    });
+});
 
 module.exports = router;
