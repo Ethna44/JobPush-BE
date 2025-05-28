5e0d3953a0565acb7ab0d1ea5d8477333a3fa17e
Ethan <ethan.frappie@gmail.com>
2025-05-28 17:02:44 +0200

    articles route get

--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,18 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const Article = require("../models/articles");
 
-router.get('/', (req, res) => {
- res.json({ result:true});
+router.get("/byCategory/:category", async (req, res) => {
+  try {
+    const category = req.params.category;
+    const articles = await Article.find({ category: category });
+    const data = articles.map((a) => ({
+      title: a.title,
+      description: a.description,
+    }));
+    res.json({ result: true, articles: data });
+  } catch (error) {
+    res.status(500).json({ result: false, error: error.message });
+  }
 });
-
 module.exports = router;
